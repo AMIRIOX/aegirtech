9326540bc39ab0a42e6006c016d1acfa51cf74a3
AMIRIOX <wumingyun2120@outlook.com>
2024-04-23 11:16:29 +0800

    [docs] add .gitignore, LICENSE

---
+++ src/main/java/com/amiriox/EnergyCrystal.java
@@ -0,0 +1,8 @@
+package com.amiriox;
+import net.minecraft.item.Item;
+
+public class EnergyCrystal extends Item {
+    public EnergyCrystal(Settings settings) {
+		super(settings);
+    }
+}
---
+++ src/main/java/com/amiriox/EnergyOre.java
@@ -0,0 +1,8 @@
+package com.amiriox;
+import net.minecraft.block.Block;
+
+public class EnergyOre extends Block {
+    public EnergyOre(Settings settings) {
+		super(settings);
+    }
+}
